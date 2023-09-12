b6a3ca754bc2cf71e97a221112d74beb596b32c9
Diogo Moura <diogoodev@gmail.com>
2023-09-12 01:10:34 +0000

    operadores

---
+++ modulo-1/tipo_de_dados.py
@@ -0,0 +1,48 @@
+# Inteiros (int) - usado para números inteiros
+
+# %%
+num = 10
+print(num)
+print(type(num))  # Saida: <class 'int'>
+
+# Números de ponto flutuante (float)
+
+# %%
+pi = 3.14
+print(pi)
+print(type(pi))  # Saida: <class 'float'>)
+
+# Strings (str) - usados para texto
+
+# %%
+greeting = "Hello, world"
+print(greeting)
+print(type(greeting))  # Saida: <class 'str'>
+
+# Booleanos (bool) - Usados
+
+# %%
+is_python_fun = True
+print(is_python_fun)
+print(type(is_python_fun))  # Saida: <class 'bool'>
+
+
+# Listas (list) - usadas para coleções ordenadas de valores
+
+# %%
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+print(type(numbers))
+
+# Tuplas (tuple) - semelhante às listas, mas imutáveis
+
+# %%
+coordinates = (10.0, 20.0)
+print(coordinates)
+print(type(coordinates))  # Saida: <class 'tuple'>
+
+# Dicionários (dict) - usados para coleções de pares
+# %%
+person = {"name": "Alice",  "age": 25}
+print(person)
+print(type(person))
---
+++ operadores.py
@@ -0,0 +1,52 @@
+
+# %%
+a = 10
+b = 20
+print("a = " + str(a)+"; b = " + str(b))
+
+# Operador ==
+if a == b:
+    print("a é igual a b")
+else:
+    print("a é diferente de b")
+
+# Operador !=
+
+if a != b:
+    print("a é diferente de b")
+else:
+    print("a é igual a b")
+
+# Operador >
+
+if a > b:
+    print("a é maior do que b")
+else:
+    print("a é menor ou igual a b")
+
+# Operador <
+
+if a < b:
+    print("a é menor do que b")
+else:
+    print("a é maior ou igual a b")
+
+
+# Operador and
+if a < 100 and b < 100:
+    print("a e b são menores que 100")
+else:
+    print("a e b são maiores que 100")
+
+# Operador or
+if a < 100 or b < 100:
+    print("a ou b são menores que 100")
+else:
+    print("a e b são maiores ou iguais a 100")
+
+# Operador not
+if not (a > b):
+    print("a não é maior que b")
+else:
+    print("a é maior que b")
+# %%
